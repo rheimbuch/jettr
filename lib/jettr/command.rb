323e92158a97daef82007d7d6b78179f7022f395
Ryan Heimbuch <rheimbuch@gmail.com>
2010-02-02 15:42:30 -0700

    Thor tasks for starting Jettr::Server

---
+++ lib/jettr/command.rb
@@ -0,0 +1,40 @@
+require 'thor'
+require 'jettr'
+require 'jettr/config'
+
+module Jettr
+  class Command < Thor
+    namespace "jettr"
+    
+    desc "start [PATH]", "Start the webapp at PATH"
+    method_option :port, :type => :numeric, :default => 8080, :alias => "-p"
+    method_option :type, :type => :string, :default => "webapp", :alias => "-w"
+    method_option :uri, :type => :string, :default => "/", :alias => "-u"
+    def start(path=".")
+      config = nil
+      config_file = File.join(path,"jettr.yaml")
+      if File.exist?(config_file)
+        puts "Loading config file: #{config_file}"
+        config = Jettr::Config.new(:config_file => config_file)
+      else
+        puts "Loading config..."
+        config = Jettr::Config.new({
+          :server => {
+            :port => options[:port]
+          },
+          :apps => [
+            {
+              :type => options[:type],
+              :app_path => path,
+              :app_uri => options[:uri]
+            }
+          ]
+        })
+      end
+      
+      server = config.create_server
+      puts "Starting Server..."
+      server.start
+    end
+  end
+end
---
+++ tasks/jettr.thor
@@ -0,0 +1,5 @@
+require 'thor'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'jettr')
+
+# Thor::Util.load_thorfile(File.join(Jettr::JETTR_HOME, 'lib', 'jettr', 'command.rb'))
+require 'jettr/command'
