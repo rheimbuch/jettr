da2e2435057463608f5e6526d1aa8fee55ae8435
Ryan Heimbuch <rheimbuch@gmail.com>
2010-02-03 15:17:27 -0700

    Support daemonization in Jettr::Command#start. Added supporting 'stop' method to allow stopping and cleanup of daemonized processes.

--- lib/jettr/command.rb
+++ lib/jettr/command.rb
@@ -1,14 +1,17 @@
 require 'thor'
 require 'jettr'
 require 'jettr/config'
+require 'fileutils'
 
 module Jettr
   class Command < Thor
     
     desc "start [PATH] [--port=, --type=, --uri=]", "Start the webapp at PATH"
-    method_option :port, :type => :numeric, :default => 8080, :alias => "-p"
-    method_option :type, :type => :string, :default => "webapp", :alias => "-w"
-    method_option :uri, :type => :string, :default => "/", :alias => "-u"
+    method_option :port, :type => :numeric, :default => 8080, :aliases => "-p"
+    method_option :type, :type => :string, :default => "webapp", :aliases => "-w"
+    method_option :uri, :type => :string, :default => "/", :aliases => "-u"
+    method_option :daemon, :type => :boolean, :default => false, :aliases => "-d"
+    method_option :pid, :type => :string
     def start(path=".")
       config = nil
       config_file = File.join(path,"jettr.yaml")
@@ -31,9 +34,41 @@ module Jettr
         })
       end
       
+      if(options[:daemon])
+        d = Jettr::Akuma::Daemon.new
+        pid_file = File.expand_path(options[:pid] || File.join(path,'tmp','run',"jettr.pid"))
+        if(d.daemonized?)
+          if File.exist?(pid_file)
+            puts "Pid file alread exists at: #{pid_file}"
+            puts "run `jettr stop #{pid_file}` to ensure the process has been stopped."
+            exit 1
+          end
+          FileUtils.mkdir_p File.dirname(pid_file)
+          puts "PID File: #{pid_file}"
+          d.init(pid_file)
+        else
+          d.daemonize()
+          exit 0
+        end
+      end
       server = config.create_server
       puts "Starting Server..."
       server.start
+    end
+    
+    desc "stop PATH_TO_APP_OR_PID", "Stops the daemonized jettr instance."
+    def stop(path='.')
+      pid_file = File.directory?(path) ? File.join(path, 'tmp','run','jettr.pid') : path
+      unless File.exist?(pid_file)
+        puts "Pid file not found: #{pid_file}"
+        exit 1
+      end
+      File.open(pid_file) do |file|
+        pid = file.read.strip.to_i
+        res = Process.kill("SIGINT", pid)
+        puts "Jettr process #{pid} was killed with response #{res}"
+      end
+      FileUtils.rm_rf pid_file
     end
   end
 end
