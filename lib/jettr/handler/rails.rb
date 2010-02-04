da01dbed665c5ca3b6e7414a088abd8bdead9023
Ryan Heimbuch <rheimbuch@gmail.com>
2010-02-04 16:45:24 -0700

    'jruby.min.runtimes' and similiar properties require the number to be passed as a string.

--- lib/jettr/handler/rails.rb
+++ lib/jettr/handler/rails.rb
@@ -15,15 +15,16 @@ module Jettr
         self.add_filter("org.jruby.rack.RackFilter", "/*", org.mortbay.jetty.Handler::DEFAULT)
         self.resource_base = "#{config.app_path}"
         self.add_event_listener(Jettr::Rack::RailsServletContextListener.new)
-        self.set_init_params(java.util.HashMap.new(
+        rack_params = java.util.HashMap.new({
           'rails.env' => config.rails.environment,
           'rails.root' => config.rails.root,
           'public.root' => config.rails.public,
           'org.mortbay.jetty.servlet.Default.relativeResourceBase' => '/public',
-          'jruby.min.runtimes' => config.rails.min_runtimes,
-          'jruby.max.runtimes' => config.rails.max_runtimes,
-          'jruby.initial.runtimes' => config.rails.min_runtimes
-        ))
+          'jruby.min.runtimes' => config.rails.min_runtimes.to_s,
+          'jruby.max.runtimes' => config.rails.max_runtimes.to_s,
+          'jruby.initial.runtimes' => config.rails.min_runtimes.to_s 
+        })
+        self.set_init_params(rack_params)
         self.add_servlet(Jetty::ServletHolder.new(Jetty::DefaultServlet.new), "/")
       end
     end
